0403025dd07ef58943484ee33c1984f5466f4c49
PNUchae <49390154+PNUchae@users.noreply.github.com>
2024-01-15 10:50:20 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, Flask!"
+    return "Hello, Web, App, Flask!"
+    
 if __name__ == '__main__':
     app.run('0.0.0.0', 80, True)
